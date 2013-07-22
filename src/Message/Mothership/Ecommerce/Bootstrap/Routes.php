3d3ce32dcca50966cc90baeeab21f733ae15654a
Thomas Marchant <thomas@message.co.uk>
2013-07-22 17:00:46 +0100

    routes and controller set up

--- src/Message/Mothership/Ecommerce/Bootstrap/Routes.php
+++ src/Message/Mothership/Ecommerce/Bootstrap/Routes.php
@@ -8,6 +8,18 @@ class Routes implements RoutesInterface
 {
 	public function registerRoutes($router)
 	{
+		$router['ms.ecom']->setPrefix('/sop');
 
+		$router['ms.ecom']->add('ms.ecom.process.new', '/new', '::Controller:Process#newOrders');
+
+		$router['ms.ecom']->add('ms.ecom.process.active', '/active', '::Controller:Process#activeOrders');
+
+		$router['ms.ecom']->add('ms.ecom.process.pick', '/pick', '::Controller:Process#pickOrders');
+
+		$router['ms.ecom']->add('ms.ecom.process.pack', '/pack', '::Controller:Process#packOrders');
+
+		$router['ms.ecom']->add('ms.ecom.process.post', '/post', '::Controller:Process#postOrders');
+
+		$router['ms.ecom']->add('ms.ecom.process.pickup', '/pickup', '::Controller:Process#pickupOrders');
 	}
 }
--- src/Message/Mothership/Ecommerce/Controller/Process.php
+++ src/Message/Mothership/Ecommerce/Controller/Process.php
@@ -3,41 +3,56 @@
 namespace Message\Mothership\Ecommerce\Controller;
 
 use Message\Cog\Controller\Controller;
+use Message\Mothership\Commerce\Order\Order;
 
 class Process extends Controller
 {
+	/**
+	 * @var \Message\Mothership\Commerce\Order\Loader
+	 */
+	protected $_loader;
+
+	/**
+	 * @todo should we consider renaming these to be consistent with what the orders are, rather than what they will be?
+	 */
+
+	public function __construct()
+	{
+		$this->_loader = $this->get('order.loader');
+	}
+
 	public function index()
 	{
-
+		return $this->redirectToRoute('ms.ecom.process.active');
 	}
 
 	public function newOrders()
 	{
-
+		$orders = $this->_loader->getOrders(/** constant for new orders */);
 	}
 
 	public function activeOrders()
 	{
-
+		$orders = $this->_loader->getOrders(/** leave blank to receive all orders */);
 	}
 
 	public function pickOrders()
 	{
-
+		$orders = $this->_loader->getOrders(/** constant for pickable orders */);
 	}
 
 	public function packOrders()
 	{
-
+		$orders = $this->_loader->getOrders(/** constant for packable orders */);
 	}
 
 	public function postOrders()
 	{
-
+		$orders = $this->_loader->getOrders(/** constant for postable orders */);
 	}
 
 	public function pickupOrders()
 	{
-
+		$orders = $this->_loader->getOrders(/** constant for picked up orders */);
 	}
 }
