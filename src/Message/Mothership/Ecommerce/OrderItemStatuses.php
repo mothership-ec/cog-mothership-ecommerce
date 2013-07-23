a0b336a9cbf5bde7b6477893dc96b39cab09fbaf
Joe Holdcroft <joe@message.co.uk>
2013-07-23 16:21:03 +0100

    Adding order & order item statuses

---
+++ src/Message/Mothership/Ecommerce/OrderItemStatuses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Message\Mothership\Ecommerce;
+
+/**
+ * Container for status codes made available to order items by the Ecommerce
+ * cogule.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+class OrderItemStatuses
+{
+	const AWAITING_PAYMENT = -200;
+	const HOLD             = -100;
+
+	const PRINTED          = 100;
+	const PICKED           = 200;
+	const PACKED           = 300;
+	const POSTAGED         = 400;
+
+	const RETURN_WAITING   = 1200;
+	const RETURN_ARRIVED   = 1300;
+	const RETURNED         = 1500;
+}
