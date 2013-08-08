1af9fa3338673746f9fd4d8a47ee909aafbc470e
Eleanor Shakeshaft <eleanor@message.co.uk>
2013-08-08 15:29:04 +0100

    Add account order details

--- src/Message/Mothership/Ecommerce/Controller/Account/Account.php
+++ src/Message/Mothership/Ecommerce/Controller/Account/Account.php
@@ -13,11 +13,42 @@ class Account extends Controller
 {
 	public function index()
 	{
-		de($this->get('user.current'));
+		// Load the user
+		$user = $this->get('user.current');
+		// Load the user addresses
+		$addresses = $this->get('commerce.user.loader')->getByUser($user);
+		// Get the most recent
+		$address = array_shift($addresses);
 
 		return $this->render('Message:Mothership:Ecommerce::Account:account', array(
-			'hello'   => 'hello',
+			'user'    => $user,
+			'address' => $address,
 		));
+	}
+
+	public function orderListing()
+	{
+		// Load the logged in user
+		$user = $this->get('user.current');
+		// return their orders
+		$orders = $this->get('order.loader')->getByUser($user);
+
+		return $this->render('Message:Mothership:Commerce::order:all-orders:summary', array(
+			'orders' => $orders,
+		));
+	}
+
+	public function orderDetail($orderID)
+	{
+		// Load the current order 
+		$order = $this->get('order.loader')->getByID($orderID);
+
+		//de($order);
+
+		return $this->render('Message:Mothership:Ecommerce::Account:orderdetails', array(
+			'order' => $order,
+		));
+		
 	}
 
 }
