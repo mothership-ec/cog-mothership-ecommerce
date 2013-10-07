aacd154f83977591e5b8d46e207e273e51adf540
Thomas Marchant <thomas@message.co.uk>
2013-10-07 14:47:34 +0100

    picking slips

--- src/Message/Mothership/Ecommerce/Controller/Fulfillment/Process.php
+++ src/Message/Mothership/Ecommerce/Controller/Fulfillment/Process.php
@@ -113,12 +113,15 @@ class Process extends Controller
 	public function pickAction($orderID)
 	{
 		$form = $this->_getPickForm($orderID, OrderItemStatuses::PRINTED);
+		$items = $this->_getOrderItems($orderID, OrderItemStatuses::PRINTED);
 
 		if ($form->isValid() && $data = $form->getFilteredData()) {
 
 			$this->_updateItemStatus($orderID, OrderItemStatuses::PICKED, $data['choices']);
 
-			$this->_saveNewPackingSlips($orderID, $data['choices']);
+			if (count($data['choices']) < count($items)) {
+				$this->_saveNewPackingSlips($orderID, $data['choices']);
+			}
 
 			if ($data['packed']) {
 				$this->_packActionOrder($orderID, $data['choices']);
