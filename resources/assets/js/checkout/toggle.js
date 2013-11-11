b4fdd529ec59d29f258d9ab0d73f4325aa20b57f
Laurence Roberts <lsjroberts@gmail.com>
2013-11-11 15:07:08 +0000

    Added delivery input required attr toggle

--- resources/assets/js/checkout/toggle.js
+++ resources/assets/js/checkout/toggle.js
@@ -13,4 +13,25 @@ $(function() {
 			target.slideDown();
 		}
 	}).trigger('change.toggle');
+
+	// Trigger the change a second time on inverted toggle inputs to ensure it
+	// is run and run twice.
+	$('input[type=checkbox][data-toggle-inverted]').trigger('change.toggle');
+
+	// On the register delivery toggle, add and remove the required attributes
+	// of the related inputs
+	$('#register_deliver_to_different').on('change.toggle', function() {
+		var self   = $(this),
+			target = $('.checkout form .delivery :input'),
+			inverted = "true" == self.attr('data-toggle-inverted'),
+			checked;
+
+		checked = self.is(':checked');
+
+		if ((checked && ! inverted) || (! checked && inverted)) {
+			target.removeAttr('required').attr('data-toggle-required', true);
+		} else {
+			target.removeAttr('data-toggle-required').attr('required', true);
+		}
+	});
 });
