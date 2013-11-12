037db3cb1abb5d0c3ee0b905d7c1d1f923443c10
Laurence Roberts <lsjroberts@gmail.com>
2013-11-12 10:38:21 +0000

    Updated toggle js to compare addresses and show/hide inputs

--- resources/assets/js/checkout/toggle.js
+++ resources/assets/js/checkout/toggle.js
@@ -1,7 +1,36 @@
 $(function() {
-	$('input[type=checkbox][data-toggle]').on('change.toggle', function() {
-		var self   = $(this),
-			target = $(self.attr('data-toggle')),
+	var addressCheckbox, sameAddress;
+
+	addressCheckbox = $('#register_deliver_to_different');
+	sameAddress = true;
+
+	// Loop each billing input and compare it's value to the equivalent
+	// delivery input.
+	$(':input[name^="register[billing]"]').each(function()
+	{
+		var billingInput = $(this), name, deliveryName, deliveryInput;
+		name = billingInput.attr('name');
+		deliveryName = billingInput.attr('name').replace('billing', 'delivery');
+		deliveryInput = $(':input[name="' + deliveryName + '"]');
+
+		if (billingInput.val() != deliveryInput.val()) {
+			sameAddress = false;
+		}
+	});
+
+	// If the checkbox is wrong compared to the addresses being the same,
+	// trigger the toggle to fix it.
+	if ((sameAddress && addressCheckbox.is(':checked')) ||
+		(! sameAddress && ! addressCheckbox.is(':checked'))
+	) {
+		addressCheckbox.prop('checked', ! addressCheckbox.is(':checked'));
+	}
+
+	// Checkbox toggle switch
+	$('input[type=checkbox][data-toggle]').on('change.toggle', function()
+	{
+		var self     = $(this),
+			target   = $(self.attr('data-toggle')),
 			inverted = "true" == self.attr('data-toggle-inverted'),
 			checked;
 
