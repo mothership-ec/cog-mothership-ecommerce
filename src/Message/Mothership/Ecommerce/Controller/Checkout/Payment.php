dbf61a46c97d87dada31e22771a25365daf5dc57
Laurence Roberts <lsjroberts@gmail.com>
2013-11-26 11:37:12 +0000

    Added debug email to when payment response is not a redirect

--- src/Message/Mothership/Ecommerce/Controller/Checkout/Payment.php
+++ src/Message/Mothership/Ecommerce/Controller/Checkout/Payment.php
@@ -95,6 +95,16 @@ class Payment extends Controller
 		    $response->redirect();
 		} else {
 			$this->addFlash('error', 'Couldn\'t connect to payment gateway');
+
+			// Send a debug message
+			$message = $this->get('mail.message');
+			$message->setTo('dev@message.co.uk');
+			$message->setSubject('Payment error on ' . $this->get('cfg')->app->name);
+			$message->setBody(
+				"An error occured when a customer tried to make a payment:\n\n\n" .
+				"Response:\n\n" .
+				print_r($response, true)
+			);
 		}
 
 		return $this->redirectToRoute('ms.ecom.checkout.confirm');
