2bedec8766e207763f544183520dd4278752ff38
Laurence Roberts <lsjroberts@gmail.com>
2013-12-16 17:29:24 +0000

    Added docblocks and namespaces to product page finder interface

--- src/Message/Mothership/Ecommerce/Finder/ProductPageFinderInterface.php
+++ src/Message/Mothership/Ecommerce/Finder/ProductPageFinderInterface.php
@@ -1,8 +1,35 @@
 <?php
 
+use Message\Mothership\CMS\Page;
+use Message\Mothership\Commerce\Product\Product;
+use Message\Mothership\Commerce\Product\Unit\Unit;
+
 interface ProductPageFinderInterface
 {
+	/**
+	 * Get the left-most page for a unit.
+	 *
+	 * @param  Unit $unit
+	 * @return Page
+	 */
 	public function getPageForUnit(Unit $unit);
+
+	/**
+	 * Get the left-most page for a product.
+	 *
+	 * @param  Product    $product
+	 * @param  array|null $options Name => Value array, e.g. 'Colour' => 'Red'
+	 * @return Page
+	 */
 	public function getPageForProduct(Product $product, array $options = null);
+
+	/**
+	 * Get all pages associated with a product.
+	 *
+	 * @param  Product    $products
+	 * @param  array|null $options  Name => Value array, e.g. 'Colour' => 'Red'
+	 * @param  int|null   $limit    Limit number of pages to find
+	 * @return Page
+	 */
 	public function getPagesForProduct(Product $products, array $options = null, $limit = null);
 }
