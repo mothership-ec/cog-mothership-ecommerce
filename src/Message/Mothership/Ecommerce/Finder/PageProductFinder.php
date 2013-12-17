7cf03df9d7600bf68eece9699b2e866afbbd1fdd
Laurence Roberts <lsjroberts@gmail.com>
2013-12-17 11:17:23 +0000

    Added page product finder

---
+++ src/Message/Mothership/Ecommerce/Finder/PageProductFinder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Message\Mothership\Ecommerce\Finder;
+
+use Message\Cog\DB;
+use Message\Mothership\CMS\Page;
+use Message\Mothership\Commerce\Product\Product;
+use Message\Mothership\Commerce\Product\Unit\Unit;
+
+class PageProductFinder implements PageProductFinderInterface
+{
+	protected $_query;
+	protected $_loader;
+	protected $_auth;
+
+	protected $_filters = array();
+
+	public function __construct(DB\Query $query, Page\Loader $loader, Page\Authorisation $auth)
+	{
+		$this->_query  = $query;
+		$this->_loader = $loader;
+		$this->_auth   = $auth;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function addFilter($callable)
+	{
+		if (!is_callable($callable)) {
+			throw new \InvalidArgumentException('Filters for PageProductFinder must be callable.');
+		}
+
+		$this->_filters[] = $callable;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getUnitsForPage(Page $page)
+	{
+		return;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getProductsForPage(Page $page)
+	{
+		return;
+	}
+}
---
+++ src/Message/Mothership/Ecommerce/Finder/PageProductFinderInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Message\Mothership\Ecommerce\Finder;
+
+use Message\Mothership\CMS\Page;
+use Message\Mothership\Commerce\Product\Product;
+use Message\Mothership\Commerce\Product\Unit\Unit;
+
+interface PageProductFinderInterface
+{
+	/**
+	 * Get the units associated with a page.
+	 *
+	 * @param  Page   $page
+	 * @return array[Unit]
+	 */
+	public function getUnitsForPage(Page $page);
+
+	/**
+	 * Get the products associated with a page.
+	 *
+	 * @param  Page   $page
+	 * @return array[Product]
+	 */
+	public function getProductsForPage(Page $page);
+}
