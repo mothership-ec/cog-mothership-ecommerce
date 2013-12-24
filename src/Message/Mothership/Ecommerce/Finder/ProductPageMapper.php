0fb14f627de1c9402452774e08f6b8c491a0c4fe
Laurence Roberts <lsjroberts@gmail.com>
2013-12-24 15:25:01 +0000

    Combined PageProductFinder and ProductPageFinder into ProductPageMapper

--- src/Message/Mothership/Ecommerce/Finder/ProductPageMapper.php
+++ src/Message/Mothership/Ecommerce/Finder/ProductPageMapper.php
@@ -4,10 +4,10 @@ namespace Message\Mothership\Ecommerce\Finder;
 
 use Message\Cog\DB;
 use Message\Mothership\CMS\Page;
-use Message\Mothership\Commerce\Product\Product;
-use Message\Mothership\Commerce\Product\Unit\Unit;
+use Message\Mothership\Commerce\Product;
+use Message\Mothership\Commerce\Product\Unit;
 
-class ProductPageFinder implements ProductPageFinderInterface
+class ProductPageFinder implements ProductPageFinderInterface, PageProductFinderInterface
 {
 	protected $_query;
 	protected $_loader;
@@ -19,14 +19,23 @@ class ProductPageFinder implements ProductPageFinderInterface
 	 * Constructor.
 	 *
 	 * @param DB\Query           $query
-	 * @param Page\Loader        $loader
-	 * @param Page\Authorisation $auth
+	 * @param Page\Loader        $pageLoader
+	 * @param Page\Authorisation $pageAuth
+	 * @param Product\Loader     $productLoader
+	 * @param Unit\Loader        $unitLoader
 	 */
-	public function __construct(DB\Query $query, Page\Loader $loader, Page\Authorisation $auth)
-	{
-		$this->_query  = $query;
-		$this->_loader = $loader;
-		$this->_auth   = $auth;
+	public function __construct(
+		DB\Query           $query,
+		Page\Loader        $pageLoader,
+		Page\Authorisation $pageAuth,
+		Product\Loader     $productLoader,
+		Unit\Loader        $unitLoader
+	) {
+		$this->_query         = $query;
+		$this->_pageLoader    = $pageLoader;
+		$this->_pageAuth      = $pageAuth;
+		$this->_productLoader = $productLoader;
+		$this->_unitLoader    = $unitLoader;
 	}
 
 	/**
@@ -44,7 +53,7 @@ class ProductPageFinder implements ProductPageFinderInterface
 	/**
 	 * @{inheritDoc}
 	 */
-	public function getPageForUnit(Unit $unit)
+	public function getPageForUnit(Unit\Unit $unit)
 	{
 		// Initially try to find the page matched against the unit's options.
 		if ($page = $this->getPageForProduct($unit->product, $unit->options)) {
@@ -59,7 +68,7 @@ class ProductPageFinder implements ProductPageFinderInterface
 	/**
 	 * @{inheritDoc}
 	 */
-	public function getPageForProduct(Product $product, array $options = null)
+	public function getPageForProduct(Product\Product $product, array $options = null)
 	{
 		$pages = $this->getPagesForProduct($product, $options, 1);
 
@@ -69,7 +78,36 @@ class ProductPageFinder implements ProductPageFinderInterface
 	/**
 	 * @{inheritDoc}
 	 */
-	public function getPagesForProduct(Product $product, array $options = null, $limit = null)
+	public function getUnitsForPage(Page\Page $page)
+	{
+		$return = array();
+
+		$products = $this->getProductsForPage($page);
+
+		$this->_unitLoader->includeInvisible(true);
+		$this->_unitLoader->includeOutOfStock(true);
+
+		foreach ($products as $product) {
+			if ($units = $this->_unitLoader->getByProduct($product)) {
+				$return += $units;
+			}
+		}
+
+		return $return;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getProductForPage(Page\Page $page)
+	{
+		return $this->getProductsForPage($page, 1);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getPagesForProduct(Product\Product $product, array $options = null, $limit = null)
 	{
 		$return = array();
 
@@ -139,6 +177,60 @@ class ProductPageFinder implements ProductPageFinderInterface
 			}
 
 			$return[$key] = $page;
+		}
+
+		return $return;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getProductsForPage(Page\Page $page, $limit = null)
+	{
+		$return = array();
+
+		$query = '
+			SELECT
+				product.product_id
+			FROM
+				product
+			JOIN
+				page_content ON (
+					page_content.value_int = product.product_id
+				AND page_content.group_name = "product"
+				AND page_content.field_name = "product"
+				)
+			LEFT JOIN
+				page ON (
+					page.page_id = page_content.page_id
+				)
+			WHERE
+				page.page_id = :pageID?i
+			ORDER BY
+				product.product_id ASC
+		';
+
+		$params = array(
+			'pageID' => $page->id,
+		);
+
+		if (null !== $limit) {
+			$query .= ' LIMIT :limit?i';
+			$params['limit'] = $limit;
+		}
+
+		$result = $this->_query->run($query, $params);
+
+		foreach ($this->_productLoader->getByID($result->flatten()) as $key => $product) {
+
+			// Run custom filters and remove any where the return value is falsey
+			foreach ($this->_filters as $filter) {
+				if (!$filter($product)) {
+					continue 2;
+				}
+			}
+
+			$return[$key] = $product;
 		}
 
 		return $return;
