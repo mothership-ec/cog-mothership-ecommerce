ef5dda82629c18e4e9fddf241654187c33683009
Laurence Roberts <lsjroberts@gmail.com>
2014-01-10 11:21:12 +0000

    Updated product page mapper twig extension

---
+++ src/Message/Mothership/Ecommerce/Templating/ProductPageMapperTwigExtension.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Message\Mothership\Ecommerce\Templating;
+
+use Twig_Extension;
+use Twig_SimpleFunction;
+use Message\Mothership\CMS\Page\Page;
+use Message\Mothership\Commerce\Product\Product;
+use Message\Mothership\Commerce\Product\Unit\Unit;
+use Message\Mothership\Ecommerce\ProductPageMapper\AbstractMapper;
+use Message\Mothership\Ecommerce\ProductPageMapper\ProductPageFinderInterface;
+use Message\Mothership\Ecommerce\ProductPageMapper\PageProductFinderInterface;
+
+class ProductPageMapperTwigExtension extends Twig_Extension implements ProductPageFinderInterface, PageProductFinderInterface
+{
+	protected $_mapper;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param AbstractMapper $mapper
+	 */
+	public function __construct(AbstractMapper $mapper)
+	{
+		$this->_mapper = $mapper;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getFunctions()
+	{
+		return array(
+			new Twig_SimpleFunction('getPageForUnit',     array($this, 'getPageForUnit')),
+			new Twig_SimpleFunction('getPageForProduct',  array($this, 'getPageForProduct')),
+			new Twig_SimpleFunction('getPagesForProduct', array($this, 'getPagesForProduct')),
+			new Twig_SimpleFunction('getUnitsForPage',    array($this, 'getUnitsForPage')),
+			new Twig_SimpleFunction('getProductForPage',  array($this, 'getProductForPage')),
+			new Twig_SimpleFunction('getProductsForPage', array($this, 'getProductsForPage')),
+		);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getName()
+	{
+		return 'product_page_mapper';
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getPageForUnit(Unit $unit)
+	{
+		return $this->_mapper->getPageForUnit($unit);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getPageForProduct(Product $product, array $options = null)
+	{
+		return $this->_mapper->getPageForProduct($product, $options);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getPagesForProduct(Product $product, array $options = null, $limit = null)
+	{
+		return $this->_mapper->getPagesForProduct($product, $options, $limit);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getUnitsForPage(Page $page)
+	{
+		return $this->_mapper->getUnitsForPage($page);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getProductForPage(Page $page)
+	{
+		return $this->_mapper->getProductForPage($page);
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public function getProductsForPage(Page $page, $limit = null)
+	{
+		return $this->_mapper->getProductsForPage($page, $limit);
+	}
+}
