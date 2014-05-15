b28aede0c2c869e9f05241f51d635f1d1eafe464
Eleanor Shakeshaft <eleanor@message.co.uk>
2014-05-15 16:11:12 +0100

    Add validation to country in checkout address form

--- src/Form/CheckoutAddressesForm.php
+++ src/Form/CheckoutAddressesForm.php
@@ -54,6 +54,22 @@ class CheckoutAddressesForm extends Form\AbstractType
 			$data['delivery']->id   = 'delivery';
 			$event->setData($data);
 		}
+
+		$this->validateCountry($event->getForm(), $event->getData());
+	}
+
+	public function validateCountry($form, $data)
+	{
+		foreach (['delivery','billing'] as $type) {
+			$event = $this->_services['country.event'];
+			$countries = $this->_services['event.dispatcher']->dispatch('country.'.$type, $event)->getCountries();
+
+			if (!isset($countries[$data[$type]->countryID]) ) {
+				$form->get($type)->get('countryID')->addError(new Form\FormError(sprintf('%s is not an available '.$type.' country.',
+					$this->_services['country.list']->getByID($data[$type]->countryID)
+				)));
+			}
+		}
 	}
 
 	public function setDefaultOptions(OptionsResolverInterface $resolver)
