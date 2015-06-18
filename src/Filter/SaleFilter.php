7475c7c0f8b125a079bbcc6650271d771e9606f5
Samuel Trangmar-Keates <sam@message.co.uk>
2015-06-18 10:11:32 +0100

    Allow pages with no options to be retrieved

--- src/Filter/SaleFilter.php
+++ src/Filter/SaleFilter.php
@@ -8,6 +8,12 @@ use Message\Mothership\Commerce\Product\Unit\Loader as UnitLoader;
 use Message\Mothership\CMS\Page\Filter\ContentFilter;
 use Message\Cog\DB\QueryBuilderFactory;
 
+/**
+ * @author Samuel Trangmar-Keates
+ *
+ * Sale filter class for the PageLoader. Filters the results to only show pages
+ * for products in the sale.
+ */
 class SaleFilter extends AbstractContentFilter
 {
 	protected $_productGroup      = 'product';
@@ -25,6 +31,9 @@ class SaleFilter extends AbstractContentFilter
 		$this->_queryBuilderFactory = $queryBuilderFactory;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public function setGroup($group)
 	{
 		$this->_productGroup = $group;
@@ -32,6 +41,9 @@ class SaleFilter extends AbstractContentFilter
 		return $this;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public function setProductField($field)
 	{
 		$this->_productFieldName = $field;
@@ -39,6 +51,9 @@ class SaleFilter extends AbstractContentFilter
 		return $this;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public function setOptionField($field)
 	{
 		$this->_optionFieldName = $field;
@@ -46,6 +61,9 @@ class SaleFilter extends AbstractContentFilter
 		return $this;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	protected function _applyFilter(QueryBuilderInterface $queryBuilder)
 	{
 		if (empty($this->_value)) {
@@ -54,10 +72,17 @@ class SaleFilter extends AbstractContentFilter
 
 		$saleUnits = $this->_unitLoader->getSaleUnits();
 
+		// if no units found then just return no pages
+		if (empty($saleUnits)) {
+			$queryBuilder->where('FALSE');
+			return;
+		}
+
 		$saleProductIDs = [];
 		$saleOptionNames = [];
 		$saleOptionValues = [];
 
+		// Build sale product variables for the query
 		foreach ($saleUnits as $unit) {
 			$saleProductIDs[$unit->getProduct()->id] = $unit->getProduct()->id;
 
@@ -75,20 +100,13 @@ class SaleFilter extends AbstractContentFilter
 
 		$contentAlias = $this->_getContentAlias();
 
+		// Subquery to select all pages with the wrong content fields to exclude.
+		// This is chosen over selecting the pages with the correct fields as
+		// we want to include pages with only a product and no options too.
 		$pageSubQuery = $subQuery()
 			->select($contentAlias . '_product.page_id')
 			->from(
 				$contentAlias . '_product',
-				$subQuery()
-					->select('page_id')
-					->from('page_content')
-					->where('page_content.field_name = ?s', [$this->_productFieldName])
-					->where('page_content.group_name = ?s', [$this->_productGroup])
-					->where('page_content.value_string IN (?ij)', [$saleProductIDs])
-			)
-			->join(
-				$contentAlias . '_product_option_name',
-				$contentAlias . '_product_option_name.page_id = ' . $contentAlias . '_product.page_id',
 				$subQuery()
 					->select('page_id')
 					->from('page_content')
@@ -106,13 +124,24 @@ class SaleFilter extends AbstractContentFilter
 					->where('page_content.field_name = ?s', [$this->_optionFieldName])
 					->where('page_content.group_name = ?s', [$this->_productGroup])
 					->where('page_content.data_name = \'value\'')
-					->where('page_content.value_string IN (?sj)', [$saleOptionValues])
+					->where('page_content.value_string NOT IN (?sj)', [$saleOptionValues])
 			)
 		;
 
 		$queryBuilder
 			->leftJoin($contentAlias, $this->_getJoinStatement(), 'page_content')
-			->where('page.page_id IN (?q)', [$pageSubQuery]);
+			// Exclude pages with incorrect options
+			->where('page.page_id NOT IN (?q)', [$pageSubQuery])
+			// Exclude pages with incorrect product
+			->where('page.page_id NOT IN (?q)', [
+				$subQuery()
+				->select('page_id')
+				->from('page_content')
+				->where('page_content.field_name = ?s', [$this->_productFieldName])
+				->where('page_content.group_name = ?s', [$this->_productGroup])
+				->where('page_content.value_string NOT IN (?ij)', [$saleProductIDs])
+			])
+		;
 
 	}
 }
